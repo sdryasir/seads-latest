a1ee9a5fcc8be29e8bcfb3b3f9cad50f33cd8763
ShahidBinShafiq <shahidvital786@gmail.com>
2024-09-19 15:58:59 +0500

    first commit

---
+++ assets/js/theme.js
@@ -0,0 +1,17 @@
+// AOS Initilization
+AOS.init();
+
+
+
+let headerSect = document.querySelector("#headerSect");
+
+let headerHeight = headerSect.offsetHeight;
+
+let heroSect = document.querySelector("#heroSect");
+
+heroSect.style.marginTop = headerHeight + "px";
+
+
+
+
+
