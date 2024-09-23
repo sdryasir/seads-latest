d91edced882ed905794df841b137bd28a782d5df
ShahidBinShafiq <shahidvital786@gmail.com>
2024-09-23 16:33:36 +0500

    merged both developers

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,9 +9,28 @@ let headerHeight = headerSect.offsetHeight;
 
 let heroSect = document.querySelector("#heroSect");
 
-heroSect.style.marginTop = headerHeight + "px";
+heroSect.style.paddingTop = headerHeight + "px";
 
 
 
 
 
+
+const navToggler = document.getElementById('navToggler');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+
+    navToggler.addEventListener('click', function () {
+        if (navbarCollapse.classList.contains('show')) {
+            document.body.style.overflow = '';
+        } else {
+            document.body.style.overflow = 'hidden';
+        }
+    });
+
+    navbarCollapse.addEventListener('hidden.bs.collapse', function () {
+        document.body.style.overflow = '';
+    });
+
+    navbarCollapse.addEventListener('shown.bs.collapse', function () {
+        document.body.style.overflow = 'hidden';
+    });
