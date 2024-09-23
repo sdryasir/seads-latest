10431ade3fec09a8d8801283d0d4e44f1332d983
ShahidBinShafiq <shahidvital786@gmail.com>
2024-09-23 14:06:14 +0500

    hello

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,9 +9,33 @@ let headerHeight = headerSect.offsetHeight;
 
 let heroSect = document.querySelector("#heroSect");
 
-heroSect.style.marginTop = headerHeight + "px";
+heroSect.style.paddingTop = headerHeight + "px";
+
+
+// let navToggler = document.querySelector("#navToggler");
+// let siteWrapper = document.querySelector("#siteWrapper");
+
+// navToggler.addEventListener('click', function(){
+//     siteWrapper.classList.toggle("scrollNone")
+// })
 
 
 
+const navToggler = document.getElementById('navToggler');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
 
+    navToggler.addEventListener('click', function () {
+        if (navbarCollapse.classList.contains('show')) {
+            document.body.style.overflow = '';
+        } else {
+            document.body.style.overflow = 'hidden';
+        }
+    });
 
+    navbarCollapse.addEventListener('hidden.bs.collapse', function () {
+        document.body.style.overflow = '';
+    });
+
+    navbarCollapse.addEventListener('shown.bs.collapse', function () {
+        document.body.style.overflow = 'hidden';
+    });
